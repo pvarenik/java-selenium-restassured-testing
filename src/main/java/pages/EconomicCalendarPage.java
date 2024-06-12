a72fa23b32664e67ff990c9f295f3b4004dfb3f6
Pavel Varenik <pavel.varenik@altoros.com>
2024-06-12 14:40:32 +0200

    Next week calendar test fixes

--- src/main/java/pages/EconomicCalendarPage.java
+++ src/main/java/pages/EconomicCalendarPage.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
 import org.testng.Assert;
 
 public class EconomicCalendarPage {
@@ -72,31 +73,34 @@ public class EconomicCalendarPage {
         wait.until(ExpectedConditions.visibilityOfElementLocated(highlightedDate));
         switch (expectedDate) {
             case Constants.TODAY:
-                compareDates(c, driver.findElement(highlightedDate), 0);
+                compareDates(c, driver.findElement(highlightedDate));
                 break;
             case Constants.TOMORROW:
                 c.setTime(new Date());
-                compareDates(c, driver.findElement(highlightedDate), 1);
+                c.add(Calendar.DATE, 1);
+                compareDates(c, driver.findElement(highlightedDate));
                 break;
             case Constants.NEXT_WEEK:
+                int daysUntilNextMonday = 7 - c.get(Calendar.DAY_OF_WEEK) + 2; // getting days to the end of the week, then adding 2 to get Monday
                 c.setTime(new Date());
+                c.add(Calendar.DATE, daysUntilNextMonday);
                 List<WebElement> dates = driver.findElements(highlightedDate);
                 Assert.assertEquals(dates.size(), 2, "Time period was displayed incorrectly.");
-                for(WebElement e: dates) {
-                   compareDates(c, e, Constants.NEXT_WEEK_DAYS);
+                for (WebElement e : dates) {
+                    compareDates(c, e);
+                    c.add(Calendar.DATE, Constants.NEXT_WEEK_DAYS);
                 }
                 break;
         }
     }
 
-    public void compareDates(@NotNull Calendar c, @NotNull WebElement e, int days) {
-        c.add(Calendar.DATE, days);
+    public void compareDates(@NotNull Calendar c, @NotNull WebElement e) {
         String dateActualText, dateActualAttribute, dateExpected, dateExpectedAttribute;
         dateExpected = new SimpleDateFormat(Constants.DATE_FORMAT_DAY).format(c.getTime());
         dateExpectedAttribute = new SimpleDateFormat(Constants.DATE_FORMAT_FULL).format(c.getTime());
         dateActualText = e.getText();
         dateActualAttribute = e.getAttribute("aria-label");
         Assert.assertEquals(dateActualAttribute, dateExpectedAttribute, "Incorrect full date.");
-        Assert.assertEquals(dateActualText, dateExpected, "Incorrect date." );
+        Assert.assertEquals(dateActualText, dateExpected, "Incorrect date.");
     }
 }
